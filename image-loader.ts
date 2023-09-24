82504aed40f39e830c036c488e65f583d9e0d275
haochengxu <haochengx@gmail.com>
2023-09-24 21:03:50 +0800

    update img

--- image-loader.ts
+++ image-loader.ts
@@ -7,6 +7,5 @@ export default function imageLoader({
     width: number
     quality?: number
   }) {
-    // const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`]
-    return src.substring(1)
+    return src
   }
