d0c82757b7374c8cb8ff5ccaeabd6b0baadb4a27
haochengxu <haochengx@gmail.com>
2023-09-25 21:37:26 +0800

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,27 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../interfaces/post'
+import Link from 'next/link'
 
-type Props = {
-  allPosts: Post[]
+const indexStyle = {
+  backgroundImage: "url('/assets/photo/AK9A0963.JPG')",
+  height: '100vh',
+  width: '100vw',
+  position: 'relative',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
 }
 
-export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index() {
   return (
     <>
-      <Layout>
-        <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
+      <div className="min-h-screen" style={indexStyle} >
+        <h3 className="text-3xl mb-3 leading-snug">
+          <Link
+            href="/blog"
+            className="hover:underline"
+          >
+            Blog
+          </Link>
+        </h3>
+      </div>
     </>
   )
-}
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
 }
